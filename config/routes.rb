0c6872ed4a5f93b5de47ed49210986ba922c05a3
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2022-08-06 09:58:09 +0900

    use resource token instead of ID

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :games
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :games, param: :token
 
-  # Defines the root path route ("/")
   root 'games#new'
 end
