1dae1712457e5764b6e321c7633669d089861180
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2022-08-11 15:08:48 +0900

    remove unnecessary methods in PlayersControler and views (#30)

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,5 @@
 class GamesController < ApplicationController
-  before_action :set_game, only: %i[show edit update destroy]
+  before_action :set_game, only: %i[show]
 
   # GET /games/1 or /games/1.json
   def show
