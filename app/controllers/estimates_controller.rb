0fdb3acbd1a8daeae6d6e568e72b5289bb985e62
Masaki Suketa <masaki.suketa@nifty.ne.jp>
2022-08-13 07:40:56 +0900

    refactoring Estimate controller to model

--- app/controllers/estimates_controller.rb
+++ app/controllers/estimates_controller.rb
@@ -2,11 +2,7 @@
 
 class EstimatesController < ApplicationController
   def create
-    @estimate = Estimate.find_or_initialize_by(player_id: session[:player_id])
-    @estimate.point = params_estimate[:point]
-    return unless @estimate.save!
-
-    @estimate.broadcast_update_to @estimate.player.game, target: "#{@estimate.player_id}_estimate"
+    Estimate.save_and_broadcast(session[:player_id], params_estimate[:point])
   end
 
   private
--- app/models/estimate.rb
+++ app/models/estimate.rb
@@ -2,4 +2,12 @@
 
 class Estimate < ApplicationRecord
   belongs_to :player
+
+  def self.save_and_broadcast(player_id, point)
+    estimate = Estimate.find_or_initialize_by(player_id:)
+    estimate.point = point
+    estimate.save!
+
+    estimate.broadcast_update_to estimate.player.game, target: "#{estimate.player_id}_estimate"
+  end
 end
